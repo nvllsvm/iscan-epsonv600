9367412ddc6880badbc5af428a148df1a9fc3f7c
Andrew Rabert <ar@nullsum.net>
2025-05-25 15:32:40 -0400

    Test

--- backend/defines.h
+++ backend/defines.h
@@ -31,20 +31,7 @@ extern "C"
 
 
 #ifndef __cplusplus
-
-/*  Use the C99 bool type if available
- */
-#ifdef HAVE_STDBOOL_H
 #include <stdbool.h>
-#else
-
-/*! A C++ Boolean type and corresponding keywords for our C code.
- */
-typedef enum {
-  false = 0,
-  true
-} bool;
-#endif
 #endif
 
 
